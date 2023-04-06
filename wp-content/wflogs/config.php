027b92e402b94fc92851ba97d3e4a25c900c4ec7
osony <osony@softserveinc.com>
2023-04-06 09:17:51 +0300

    init repo add files

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"2R8&>$sVv(2qj7]@YmAY~2s9b>VaK^GM1}=O>jb:v;hnI7j. @(fPjF AeEK<777";s:7:"version";s:5:"1.0.4";s:13:"attackDataKey";i:637;}
